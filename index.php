99508433c06989a714d5c048b5b2554eb2f3221f
Rip0207 <ric.poulet@gmail.com>
2015-11-15 19:44:42 +0100

    Affichage Smartphone
    Réglages pour l'affichage d'un planche spéciale pour smartphone

--- index.php
+++ index.php
@@ -51,14 +51,41 @@
 			?>
 			<!-- Interfaces -->
 			<?php
-				require_once("./Pages/Interfaces.php");
-				
-				/*require_once("./Pages/P0.php");*/
+				require_once("./Pages/Interfaces.php");				
 			?>
 			
 			
 		</div>
-		
+		<div id="Smartphone" class="color_10">
+			<div id = "P_-1" class="Planche">
+				<div class="Container">
+
+					<h2>Bienvenue sur Mon album de vie</h2>			
+					<p>
+						Du temps est encore nécessaire pour <br />
+						vous offrir le nouvel album de vie.
+					</p>
+					<img alt="Icone 'en chantier'" src="./Design/Images/Image_01.png" >
+					<br>
+					<br>
+					<a href="action('Auth\AuthController@getLogout)" class="">À très vite</a>
+					<br>
+					<div id="Paypal">
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input name="cmd" type="hidden" value="_s-xclick" />
+							<input name="hosted_button_id" type="hidden" value="6VXX4T2LPWYLJ" />
+							<input alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !" name="submit" src="https://www.paypalobjects.com/fr_FR/BE/i/btn/btn_donateCC_LG.gif" type="image" />
+							<img src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" alt="" width="1" height="1" border="0" />
+						</form>
+						IBAN BE29 5230 8057 6864 <br>
+						BIC TRIOBEBB <br>
+					</div>
+					
+				</div>
+			</div>	
+					<div id="Footer">			 
+						© Mon album de vie, une initiative de la Fondation d'utilité publique La mort fait partie de la vie 2015
+					</div>			
+		</div>
 		<!-- Scripts -->
 		<?php
 			require_once("./Pages/Scripts.php");
